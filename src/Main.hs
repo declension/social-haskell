d91d0f8f60cabf51a3e6c896d5139731d728c064
Nick B <nick@declension.net>
2015-06-04 13:25:41 +0100

    Make usernames explicit in parsing!
    Also
      * users have a store, and shortened the getters
      * add read parser
      * add `Run` class for runnable things like Command, not sure why really

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,14 @@
 module Main where
 
 import           System.IO
---import           Text.Printf (printf)
---import           Data.Maybe
+import           Data.Maybe
 import qualified Data.Foldable as Foldable (mapM_)
 import           Control.Monad (unless)
-
 import qualified Text.Parsec as P
-
 -- The error message infix operator
 import Text.Parsec ((<?>))
-
-import Control.Applicative ((<$>), (<$), (<*), (<|>))
+import Control.Applicative --((<$>), (<$), (<*), (<*>), (*>), (<|>))
+import Data.List (find)
 
 -- Get the Identity monad from here:
 import Control.Monad.Identity()
@@ -20,41 +17,64 @@ import Control.Monad.Identity()
 parse rule = P.parse rule "(input)"
 
 data User = User {
-          getName       :: String
-        , getPosts      :: [String]
-        , getFollowing  :: [User]
+          name       :: String
+        , posts      :: [String]
+        , following  :: [User]
         } deriving (Eq, Ord, Show)
+newUser = User {name=undefined, posts=[], following=[]}
 
-data Command = Post String | Read | Wall String | Follow User | Exit
+data Command = Post User String | Read User | Wall User | Follow User User | Invalid String | Exit
     deriving (Show, Eq)
 
 type Output = Maybe String
 
-runCmd :: Command -> Output
-runCmd c = Just ("Processing command: " ++ show c)
+class Run a where
+    run :: a -> Output
 
+instance Run Command where
+    run c = Just ("Processing command: " ++ show c)
+
+users :: [User]
+users = [newUser{ name="Alice" }]
+--users = []
 
 usernameParser :: P.Parsec String () String
 usernameParser = P.many1 P.alphaNum
 
+findUserParser :: P.Parsec String () User
+findUserParser = do
+        userName <- P.choice [P.string (name u) | u <- users]
+        --return (fromMaybe (P.unexpected "unknown user") $ find (\u -> name u == userName) users)
+        return $ fromJust $ find (\u -> name u == userName) users
+
 wallParser :: P.Parsec String () Command
-wallParser = Wall <$> usernameParser <* P.spaces <* P.string "wall"
+wallParser = do
+    --let user = newUser { name="foo" }
+    user <- findUserParser
+    P.skipMany1 P.space
+    P.string "wall"
+    return $ Wall user
+
+readParser :: P.Parsec String () Command
+readParser = Read <$> findUserParser
+
+cmd :: Command
+cmd = Wall newUser{ name="foo" }
 
 exitParser :: P.Parsec String () Command
-exitParser = Exit <$ P.try (P.string "exit" <|> P.string "quit" <?> "An exit token")
-
+exitParser = Exit <$ P.try (P.string "exit" <|> P.string "quit")
 
 redirect :: IO ()
 redirect = do
     putStr "> "
     input <- getLine
-    let result = parse (P.try wallParser <|> exitParser) input
+    let result = parse (P.try readParser <|> wallParser <|> exitParser <* (P.skipMany1 P.space <|> P.eof)) input
     case result of
-        Right command   -> do
-            let output = runCmd command
+        Right command -> do
+            let output = run command
             Foldable.mapM_ putStrLn output
             unless (command == Exit) redirect
-        Left  err       -> do
+        Left err -> do
             putStrLn ("error: " ++ show err)
             redirect
 
